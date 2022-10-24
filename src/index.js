b5298c1bde3f8a966e55f27c547c307c7daac9ce
Mohamed Sawah <sawah810@gmail.com>
2022-10-24 07:37:07 +0200

    add mongo container with mongo express container

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+const mongoose = require("mongoose");
 
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo";
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => console.log("connect success"))
+  .catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to our docker project </h1>");
 });
